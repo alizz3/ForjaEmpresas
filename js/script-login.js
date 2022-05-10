020cf69c80b5c6e83f9c0ffedf375692d86a5033
Alizz M <alizz.mejia03@gmail.com>
2022-05-10 14:18:39 -0500

    add Java Script for login

---
+++ js/script-login.js
@@ -0,0 +1,23 @@
+// script login
+
+document.querySelector('#button-log-ingresar').addEventListener('click', validar);
+
+function validar() {
+    var usuario = document.getElementById('usuario').value;
+    let password = document.getElementById('password').value;
+    
+    console.log(password);
+    if (usuario == "Admin" && password == "Kuepa2022") {
+        window.location = "dashboard.html";
+    }
+    else {
+        alert('error')
+    }
+}
+
+document.querySelector("#button-guardar").addEventListener('click'.guardar);
+
+function guardar() {
+    window.alert = "dashboard.html";
+}
+
