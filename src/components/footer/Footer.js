48e472fe1796dcba7b47b64b95896bb9d45564a5
Alizz M <alizz.mejia03@gmail.com>
2022-05-18 00:55:22 -0500

    Footer component is created
    js and styles are added

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <footer id="footer">
+            <h6>© 2022 FORJA EMPRESAS</h6>
+        </footer>
+    )
+}
+
+export default Footer
